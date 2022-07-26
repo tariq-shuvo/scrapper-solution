01953c3221c73883952d1cc99acd888feb2e626d
Tariqul Islam Shuvo <tariq10043@gmail.com>
2022-07-26 21:37:29 +0600

    complete the issue

--- routes/scrapping.js
+++ routes/scrapping.js
@@ -7,16 +7,55 @@ var router = express.Router();
 router.post('/loan', async (req, res, next) => {
   const {url} = req.body
 
+  const result = {} 
+
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(
+      {
+        headless: false,
+        ignoreHTTPSErrors: true,
+      }
+    );
     const page = await browser.newPage();
     const navigationPromise = page.waitForNavigation();
 
-    await page.goto(url, { waitUntil: "domcontentloaded" });
+    await page.goto(url, {
+      waitLoad: true, 
+      waitNetworkIdle: true // defaults to false
+    });
     await page.setViewport({ width: 1440, height: 744 });
     await navigationPromise;
 
-    // await page.waitForSelector('.mb-1 .bg-yawwwtwo-blue-700', {timeout: 10000});
+    await page.waitForSelector('div>.items-start', {timeout: 1000000, waitNetworkIdle: true});
+
+    const content = await page.content();
+
+    /**
+     * Load content in cheerio.
+     */
+    const $ = cheerio.load(content);
+
+    let titles = ['howrare', 'moonrank']
+    let titles_two = ['funding', 'duration', 'LTF']
+
+    $('.flex > div > .flex > .font-semibold').slice(0, 2).each((idx, elem) => {
+      const value = $(elem).text();
+      result[titles[idx]] = value;
+    })
+
+    $('.mt-4 div .font-semibold').slice(0, 1).each((idx, elem) => {
+      const value = $(elem).text();
+      result[titles_two[idx]] = value;
+    })
+
+    $('.mt-4 > div > .text-base').slice(0, 1).each((idx, elem) => {
+      const value = $(elem).text();
+      result[titles_two[idx + 1]] = value;
+    })
+
+    res.json(result);
+    // close everything
+    await page.close();
 
     // console.log(page)
     // await page.waitForSelector('ul li h3 a');
